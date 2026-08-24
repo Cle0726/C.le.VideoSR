3eff5da71e201fa34b8335a1c868b6778f35b29e
Cle0726 <kk07266417@gmail.com>
2026-08-24 10:33:03 +0800

    feat: expose media probe command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,23 @@
 mod core;
 
 use core::hardware::{detect_hardware_info, HardwareInfo};
+use core::media::{probe_media as inspect_media, MediaProbe};
+use std::path::PathBuf;
 
 #[tauri::command]
 fn detect_hardware() -> HardwareInfo {
     detect_hardware_info()
 }
 
+#[tauri::command]
+fn probe_media(path: String) -> Result<MediaProbe, String> {
+    inspect_media(&PathBuf::from(path))
+}
+
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![detect_hardware])
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![detect_hardware, probe_media])
         .run(tauri::generate_context!())
         .expect("error while running C.le.VideoSR");
 }
